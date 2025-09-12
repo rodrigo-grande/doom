3bd06ebe949c81e9c3d5b85ef3b3f74b0d2fabde
rodrigo-grande <rodrigo.grande@escola.pr.gov.br>
2025-09-12 12:15:01 -0300

    a

---
+++ doom.js
@@ -0,0 +1,5 @@
+function Mudarcor(novaCor) {
+        var elemento = document. getElementbyid("para1");
+        elemento.style.backgroundcolor = novaCor;
+        elemento.style.display = 'inline';
+    };
