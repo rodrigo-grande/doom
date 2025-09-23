b775811c493c626ccd8f063585069c3be64d3442
rodrigo-grande <rodrigo.grande@escola.pr.gov.br>
2025-09-23 08:39:31 -0300

    a

--- doom.js
+++ doom.js
@@ -3,3 +3,21 @@ function Mudarcor(novaCor) {
         elemento.style.backgroundcolor = novaCor;
         elemento.style.display = 'inline';
     };
+
+    const form = document.getElementById('texto')
+    const section = document.getElementById('sc')
+
+    form.addEventListener("submit", (event)=>{
+        event.preventDefault();
+        var input=form.querySelector('input')
+        if(parseInt(input.value) < 24 ){
+            p.innerText = "Professor é mais velho"
+            p.style.display = 'block'
+            }else{
+                var p = section.querySelector('p')
+                p.innerHTML = '<h1> Olá Mundo </h1>'
+                p.style.display = 'block'
+            } 
+        });
+
+        
