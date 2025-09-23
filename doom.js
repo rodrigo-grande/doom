6e40b52f38138e0c47e945af9b1a41f295650994
rodrigo-grande <rodrigo.grande@escola.pr.gov.br>
2025-09-23 09:15:12 -0300

    a

--- doom.js
+++ doom.js
@@ -3,6 +3,16 @@ function Mudarcor(novaCor) {
         elemento.style.backgroundcolor = novaCor;
         elemento.style.display = 'inline';
     };
+const rodri_ciculo = document.getElementById('ciculo')
+let tamanho_rodri = 1
+
+
+rodri_ciculo.addEventListener ('dblclick',()=>{
+   rodri_ciculo.style.borderRadius = `${tamanho_rodri}px`;
+   rodri_ciculo.style.width = `${tamanho_rodri*2}px`;
+   rodri_ciculo.style.height = `${tamanho_rodri*2}px`;
+   tamanho_rodri +=30;
+})
 
     const form = document.getElementById('texto')
     const section = document.getElementById('sc')
@@ -18,6 +28,14 @@ function Mudarcor(novaCor) {
                 p.innerHTML = '<h1> Olá Mundo </h1>'
                 p.style.display = 'block'
             } 
+        });
+
+        const container = document.getElementById('fotos')
+        const imagens = container.getElementsByTagName('img');
+        container.addEventListener('click', () => {
+            for (let i = 0; i < imagens.length; i++) {
+                imagens[i].src = `baixados(${i = 5}).jpg`;
+            }
         });
         
 
